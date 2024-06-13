399a49cb1b7486a6307912fd727d06e591a86eeb
Adrián Maza <adri_mv1@hotmail.com>
2024-06-13 17:31:45 +0200

    random classes deleted

--- src/com/ironahack/w1/d4/Menu.java
+++
@@ -1,7 +0,0 @@
-package com.ironahack.w1.d4;
-
-public class Menu {
-    public static void getMenu() {
-        System.out.println("Displaying menu...");
-    }
-}
--- src/com/ironahack/w1/d4/UserCreation.java
+++
@@ -1,5 +0,0 @@
-package com.ironahack.w1.d4;
-
-public class UserCreation {
-    public String userName;
-}
